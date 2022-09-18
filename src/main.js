4cca4ab8c4fd2f27298b8f00edba1f5eb57e3476
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2022-09-18 15:39:33 +0700

    feat: add font

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import { createApp } from 'vue';
+import myVueUi from '@ibrahimanshor/my-vue-ui';
+
+import '@ibrahimanshor/my-vue-ui/dist/my-vue-ui.css';
 import './style.css';
+
 import App from './App.vue';
 
-createApp(App).mount('#app');
+const app = createApp(App);
+
+app.use(myVueUi);
+
+app.mount('#app');
